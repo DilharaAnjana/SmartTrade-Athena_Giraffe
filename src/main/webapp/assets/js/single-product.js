add3b497ca7e51769832758e18e4e9fecc182a72
Anjana Dilhara <122281334+CCapitalTech@users.noreply.github.com>
2025-12-04 11:53:18 +0530

    Update ui

--- src/main/webapp/assets/js/single-product.js
+++ src/main/webapp/assets/js/single-product.js
@@ -26,7 +26,27 @@ async function loadSingleProduct() {
         const response = await fetch(`api/products/single-product?productId=${productId}`);
         if (response.ok) {// check status 200
             const data = await response.json();
-            console.log(data);
+            const product = data.singleProduct;
+            product.images.forEach((image, index) => {
+                let imgTag = document.getElementById(`image${index + 1}`);
+                let thumbImageTag = document.getElementById(`thumb-image${index + 1}`);
+                imgTag.src = image;
+                thumbImageTag.src = image;
+            });
+            document.getElementById("product-title").innerHTML = product.title;
+            document.getElementById("published-on").innerHTML = product.stockDTOList[0].createdAt;
+            document.getElementById("product-price").innerHTML = new Intl.NumberFormat("en-US", {
+                minimumFractionDigits: 2
+            }).format(product.stockDTOList[0].price);
+            document.getElementById("brand-name").innerHTML = product.brandName;
+            document.getElementById("model-name").innerHTML = product.modelName;
+            document.getElementById("product-quality").innerHTML = product.qualityValue;
+            document.getElementById("product-stock").innerHTML = product.stockDTOList[0].qty;
+            document.getElementById("color-background").innerHTML = product.colorValue;
+            document.getElementById("product-storage").innerHTML = product.storageValue;
+            document.getElementById("product-description").innerHTML = product.description;
+
+
         } else {
             Notiflix.Notify.failure("Single product data loading failed!", {
                 position: 'center-top'
