4af5bd9ca0c26f0e6c9141491697a82958acaf29
Anjana Dilhara <122281334+CCapitalTech@users.noreply.github.com>
2025-12-15 16:25:53 +0530

    Make pending order method and generate PayHere required informations

--- src/main/java/lk/jiat/smarttrade/service/CheckoutService.java
+++ src/main/java/lk/jiat/smarttrade/service/CheckoutService.java
@@ -2,12 +2,18 @@ package lk.jiat.smarttrade.service;
 
 import com.google.gson.JsonObject;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
 import lk.jiat.smarttrade.dto.*;
 import lk.jiat.smarttrade.entity.*;
 import lk.jiat.smarttrade.util.AppUtil;
+import lk.jiat.smarttrade.util.Env;
 import lk.jiat.smarttrade.util.HibernateUtil;
+import lk.jiat.smarttrade.util.PayHereUtil;
+import lk.jiat.smarttrade.validation.Validator;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -129,12 +135,11 @@ public class CheckoutService {
             if (dto.getUserAddress() != null || !dto.getCityList().isEmpty()) {
                 dto.setStatus(true);
             }
-
-            return AppUtil.GSON.toJson(dto);
+            status = true;
+            responseObject.add("checkoutData", AppUtil.GSON.toJsonTree(dto));
 
         } catch (Exception e) {
             e.printStackTrace();
-            status = false;
             message = "Something went wrong while loading checkout data.";
         } finally {
             hibernateSession.close();
@@ -147,8 +152,189 @@ public class CheckoutService {
     }
 
     public String processCheckout(CheckoutRequestDTO dto, HttpServletRequest request) {
-        return null;
+        JsonObject responseObject = new JsonObject();
+        boolean status = false;
+        String message = "";
+
+        Session hibernateSession = HibernateUtil.getSessionFactory().openSession();
+        User sessionUser = (User) request.getSession().getAttribute("user");
+        if (sessionUser == null) {
+            message = "Session expired! Please login again";
+        } else {
+            User user = hibernateSession.find(User.class, sessionUser.getId());
+            if (dto.isCurrentAddress()) {
+                Address address = hibernateSession.createQuery("FROM Address a WHERE a.user=:user AND a.isPrimary=:primary", Address.class)
+                        .setParameter("user", user)
+                        .setParameter("primary", dto.isCurrentAddress())
+                        .getSingleResultOrNull();
+                if (address == null) {
+                    message = "Address not found. Please entered address again!";
+                } else {
+                    if (!address.isPrimary()) {
+                        message = "Given address is not your primary address!";
+                    } else {
+                        // order pending method calling here
+                        Order pendingOrder = createPendingOrder(user, hibernateSession, address);
+//                        System.out.println(pendingOrder.getId());
+                        PayHereDTO paymentDetails = createPaymentDetails(hibernateSession, pendingOrder);
+                        responseObject.add("PayHere", AppUtil.GSON.toJsonTree(paymentDetails));
+                        status = true;
+                    }
+                }
+            } else {
+                if (dto.getFirstName().isBlank()) {
+                    message = "First name is required!";
+                } else if (dto.getLastName().isBlank()) {
+                    message = "Last name is required!";
+                } else if (!dto.getCitySelect().matches(Validator.IS_INTEGER)) {
+                    message = "Invalid city. Please select correct city!";
+                } else if (Integer.parseInt(dto.getCitySelect()) == AppUtil.DEFAULT_SELECTOR_VALUE) {
+                    message = "Invalid city. Please select correct city!";
+                } else if (dto.getLineOne().isBlank()) {
+                    message = "Address line one is required!";
+                } else if (dto.getPostalCode().isBlank()) {
+                    message = "Postal code is required!";
+                } else if (dto.getPostalCode().matches(Validator.POSTAL_CODE_VALIDATION)) {
+                    message = "Enter correct postal code!";
+                } else if (dto.getMobile().isBlank()) {
+                    message = "Mobile number is required!";
+                } else if (dto.getMobile().matches(Validator.MOBILE_VALIDATION)) {
+                    message = "Enter valid mobile number!";
+                } else {
+
+                    City city = hibernateSession.find(City.class, Integer.parseInt(dto.getCitySelect()));
+                    if (city == null) {
+                        message = "City not found. Please check correct city!";
+                    } else {
+                        // if someone add address except tick checkbox we have to change primary address from current to entered details
+                        // so, we have to change current primary address before entering the new address for the prevention two primary addresses
+
+                        Address existingPrimary = hibernateSession.createQuery("FROM Address a WHERE a.isPrimary=:primary AND a.user=:user", Address.class)
+                                .setParameter("primary", true)
+                                .setParameter("user", user)
+                                .getSingleResultOrNull();
+                        if (existingPrimary != null) {
+                            // primary address found
+                            existingPrimary.setPrimary(false);
+                            hibernateSession.merge(existingPrimary);
+                        } else {
+                            Address address = new Address();
+                            address.setPrimary(true);
+                            address.setLineOne(dto.getLineOne());
+                            address.setLineTwo(dto.getLineTwo());
+                            address.setPostalCode(dto.getPostalCode());
+                            address.setMobile(dto.getMobile());
+                            address.setCity(city);
+                            hibernateSession.persist(address);
+                            // order pending metho calling here
+                            Order pendingOrder = createPendingOrder(user, hibernateSession, address);
+                            PayHereDTO paymentDetails = createPaymentDetails(hibernateSession, pendingOrder);
+                            responseObject.add("PayHere", AppUtil.GSON.toJsonTree(paymentDetails));
+                            status = true;
+                        }
+                    }
+                }
+            }
+
+        }
+        hibernateSession.beginTransaction().commit();
+        hibernateSession.close();
+        responseObject.addProperty("status", status);
+        responseObject.addProperty("message", message);
+        return AppUtil.GSON.toJson(responseObject);
     }
 
+    private PayHereDTO createPaymentDetails(Session hibernateSession, Order o) {
+        // app.public.url - Ngrok url for the https connection. this is proxy url that replace the localhost url
+        String order_id = "#000" + o.getId();
+        String return_url = Env.get("app.public.url") + "/smarttrade/api/payments/return";
+        String cancel_url = Env.get("app.public.url") + "/smarttrade/api/payments/cancel";
+        String notify_url = Env.get("app.public.url") + "/smarttrade/api/payments/notify";
 
+        Order order = hibernateSession.find(Order.class, o.getId());
+        User user = hibernateSession.find(User.class, order.getUser().getId());
+        Address address = hibernateSession.createQuery("FROM Address a WHERE a.user=:user AND a.isPrimary=:primary", Address.class)
+                .setParameter("user", user)
+                .setParameter("primary", true)
+                .getSingleResult();
+        String userAddress = address.getLineOne();
+        if (!address.getLineTwo().isBlank()) {
+            userAddress += ", " + address.getLineTwo();
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        double amount = 0;
+        List<OrderItem> orderItemList = hibernateSession.createQuery("FROM OrderItem oi WHERE oi.order=:order", OrderItem.class)
+                .setParameter("order", order)
+                .getResultList();
+        for (OrderItem oi : orderItemList) {
+            if (!stringBuilder.isEmpty()) {
+                stringBuilder.append(", ");
+            }
+            stringBuilder.append(oi.getStock().getProduct().getTitle())
+                    .append(" x ")
+                    .append(oi.getQty());
+
+
+            amount += oi.getQty() * oi.getStock().getPrice();
+        }
+        amount += order.getDeliveryType().getPrice();
+
+        // Generate hash
+        String hash = PayHereUtil.generateHash(order_id, amount, AppUtil.MAIN_APP_CURRENCY);
+
+        PayHereDTO payHereDTO = new PayHereDTO();
+        payHereDTO.setSandbox(true);
+        payHereDTO.setMerchantId(PayHereUtil.getMerchantId());
+        payHereDTO.setReturnURL(return_url);
+        payHereDTO.setCancelURL(cancel_url);
+        payHereDTO.setNotifyURL(notify_url);
+        payHereDTO.setOrderId(order_id);
+        payHereDTO.setItems(stringBuilder.toString());
+        payHereDTO.setAmount(new DecimalFormat("0.00").format(amount));
+        payHereDTO.setCurrency(AppUtil.MAIN_APP_CURRENCY);
+        payHereDTO.setHash(hash);
+        payHereDTO.setFirstName(user.getFirstName());
+        payHereDTO.setLastName(user.getLastName());
+        payHereDTO.setEmail(user.getEmail());
+        payHereDTO.setPhone(address.getMobile());
+        payHereDTO.setAddress(userAddress);
+        payHereDTO.setCity(address.getCity().getName());
+        payHereDTO.setCountry(AppUtil.APP_COUNTRY);
+        return payHereDTO;
+    }
+
+    private Order createPendingOrder(User user, Session hiberSession, Address userAddress) {
+        /* I get "within city == colombo" because one order has many items with different sellers and different addresses.
+         So it's very complicated calculate delivery payment.
+         */
+        DeliveryType withinCity = hiberSession.createNamedQuery("DeliveryType.findByName", DeliveryType.class)
+                .setParameter("name", DeliveryType.Value.WITHIN_CITY.toString()).getSingleResult();
+        DeliveryType outOfCity = hiberSession.createNamedQuery("DeliveryType.findByName", DeliveryType.class)
+                .setParameter("name", DeliveryType.Value.OUT_OF_CITY.toString()).getSingleResult();
+        Status pendingStatus = hiberSession.createNamedQuery("Status.findByValue", Status.class)
+                .setParameter("value", String.valueOf(Status.Type.PENDING)).getSingleResult();
+        Order order = new Order();
+        order.setUser(user);
+        order.setStatus(pendingStatus);
+        if (userAddress.getCity().getName().equalsIgnoreCase("Colombo")) {
+            order.setDeliveryType(withinCity);
+        } else {
+            order.setDeliveryType(outOfCity);
+        }
+        hiberSession.persist(order);
+        List<Cart> cartList = hiberSession.createQuery("FROM Cart c WHERE c.user=:user", Cart.class)
+                .setParameter("user", user)
+                .getResultList();
+        for (Cart cart : cartList) {
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            orderItem.setRating(0);
+            orderItem.setStock(cart.getStock());
+            orderItem.setQty(cart.getQty());
+            hiberSession.persist(orderItem);
+        }
+        hiberSession.beginTransaction().commit();
+        return order;
+    }
 }
